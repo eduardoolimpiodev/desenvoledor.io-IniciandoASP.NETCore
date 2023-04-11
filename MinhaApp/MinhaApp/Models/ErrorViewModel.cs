7b3364788459aa4e2ae839309aaf3a1ee2245b03
Eduardo Olimpio <106188291+eduardoolimpiodev@users.noreply.github.com>
2023-04-11 11:31:01 -0300

    add: Project

---
+++ MinhaApp/MinhaApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MinhaApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
